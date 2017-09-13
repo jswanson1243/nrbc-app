8b15fb1edc06be0eadd820916cce72c65127612f
rails udemy course <john.s.swanson.mail@gmail.com>
2017-09-13 20:08:55 +0000

    Updated display to show trip report

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :about]
+  skip_before_action :authenticate_user!, only: [:index, :about, :articles]
   def index
   end
 end
