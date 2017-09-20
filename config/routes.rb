f90e2e0bd14029b86b5783337a8e09bfa534a129
rails udemy course <john.s.swanson.mail@gmail.com>
2017-09-20 18:27:35 +0000

    fixed about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   devise_for :users
   root 'welcome#index'
-  get 'about', to: 'pages#about'
+  get 'about', to: 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
